9a985c2a1bc23d9fa26e1e18ece9ca2336862109
Nattakarn Klongyut <karn806@Nattakarns-MacBook-Air.local>
2025-01-02 14:09:35 +0700

    initial commit

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h2>Nubnib</h2>
+            <p>i have no life T_T</p>
+        </div>
+    );
+}
+
+export default Card
