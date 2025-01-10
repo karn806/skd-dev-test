c762cf03bd54cb99852dffe6f7d5b6f87befd92b
Nattakarn Klongyut <karn806@Nattakarns-MacBook-Air.local>
2025-01-10 15:10:15 +0700

    fix css

--- my-react-app/src/CardFooter.jsx
+++ my-react-app/src/CardFooter.jsx
@@ -1,9 +1,21 @@
 /* eslint-disable react/prop-types */
+import IconButton from "@mui/material/IconButton";
+import IosShareIcon from '@mui/icons-material/IosShare';
+import PersonIcon from '@mui/icons-material/Person';
+
 function CardFooter(props) {
     return (
-        <div className="card-footer">
-            <div className="card-footer-left">
+        <div className="content-left-right">
+            <div className="content-left">
+                <PersonIcon/>
                 <p className="card-footer-text">{props.likes} คนที่สนใจ</p>
+            </div>
+            <div>
+                <p>
+                    <IconButton color="primary" aria-label="add">
+                        <IosShareIcon />
+                    </IconButton>
+                </p>
             </div>
         </div>
     );
--- my-react-app/src/Content.jsx
+++ my-react-app/src/Content.jsx
@@ -1,38 +1,50 @@
 /* eslint-disable react/prop-types */
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+
 function Content(props) {
   return (
     <div>
       <div className="content-left-right">
         <div className="content-left">
-          <h4>รอบที่เปิด: {props.roundSeats.map((seat, index) => (
+          <p>รอบที่เปิด: {props.roundSeats.map((seat, index) => (
             <span key={index}>
               {seat}{index < props.roundSeats.length - 1 && '  '}
             </span>
-          ))}</h4>
+          ))}</p>
         </div>
       </div>
 
       <div className="content-left-right">
         <div className="content-left">
-          <p>รอบที่ 4: {props.score ? props.score.scoreType : "ไม่มีข้อมูล"}</p>
+          <h4>รอบที่ 4: {props.score ? props.score.scoreType : "ไม่มีข้อมูล"}</h4>
         </div>
         <div className="content-right">
           <button className="card-footer-button">แก้ไขคะแนน</button>
         </div>
       </div>
 
+      <div className="content-left-right">
+        <div className="content-left">
+          <WorkspacePremiumIcon/>
+        </div>
+        <div className="your-score-container">
+          <div className="your-score-label">คะแนนของคุณคือ</div>
+          <div className="your-score-value">{props.score ? "23,456" : "ไม่มีข้อมูล"}</div>
+        </div>
+      </div>
+
       <div className="content-bottom">
-        <div className="text-with-sub">
-          <div className="main-text">{props.score ? props.score.min : "ไม่มีข้อมูล"}</div>
-          <div className="subtext">คะแนนต่ำสุด</div>
+        <div className="score-container">
+          <div className="score-value">{props.score ? props.score.min : "ไม่มีข้อมูล"}</div>
+          <div className="score-label">คะแนนต่ำสุด</div>
         </div>
-        <div className="text-with-sub">
-          <div className="main-text">{props.score ? props.score.avg : "ไม่มีข้อมูล"}</div>
-          <div className="subtext">คะแนนเฉลี่ย</div>
+        <div className="score-container">
+          <div className="score-value">{props.score ? props.score.avg : "ไม่มีข้อมูล"}</div>
+          <div className="score-label">คะแนนเฉลี่ย</div>
         </div>
-        <div className="text-with-sub">
-          <div className="main-text">{props.score ? props.score.max : "ไม่มีข้อมูล"}</div>
-          <div className="subtext">คะแนนสูงสุด</div>
+        <div className="score-container">
+          <div className="score-value">{props.score ? props.score.max : "ไม่มีข้อมูล"}</div>
+          <div className="score-label">คะแนนสูงสุด</div>
         </div>
       </div>
     </div>
