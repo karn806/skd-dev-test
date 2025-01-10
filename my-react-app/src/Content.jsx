d490f41fb2b87290c5c22a4c72a88e23b43934ed
Nattakarn Klongyut <karn806@Nattakarns-MacBook-Air.local>
2025-01-10 15:28:36 +0700

    implement the roundseats display

--- my-react-app/src/Content.jsx
+++ my-react-app/src/Content.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import LooksOneOutlinedIcon from '@mui/icons-material/LooksOneOutlined';
+import LooksTwoOutlinedIcon from '@mui/icons-material/LooksTwoOutlined';
+import Looks3OutlinedIcon from '@mui/icons-material/Looks3Outlined';
+import Looks4OutlinedIcon from '@mui/icons-material/Looks4Outlined';
+import Looks5OutlinedIcon from '@mui/icons-material/Looks5Outlined';
+
 
 function Content(props) {
   return (
     <div>
       <div className="content-left-right">
         <div className="content-left">
-          <p>รอบที่เปิด: {props.roundSeats.map((seat, index) => (
-            <span key={index}>
-              {seat}{index < props.roundSeats.length - 1 && '  '}
-            </span>
-          ))}</p>
+          <p>รอบที่เปิด</p>
+          <LooksOneOutlinedIcon style={{ color: props.roundSeats[0] > 0 ? 'green' : 'black' }}></LooksOneOutlinedIcon>
+          <LooksTwoOutlinedIcon style={{ color: props.roundSeats[1] > 0 ? 'green' : 'black' }}></LooksTwoOutlinedIcon>
+          <Looks3OutlinedIcon style={{ color: props.roundSeats[2] > 0 ? 'green' : 'black' }}></Looks3OutlinedIcon>
+          <Looks4OutlinedIcon style={{ color: props.roundSeats[3] > 0 ? 'green' : 'black' }}></Looks4OutlinedIcon>
+          <Looks5OutlinedIcon style={{ color: props.roundSeats[4] > 0 ? 'green' : 'black' }}></Looks5OutlinedIcon>
         </div>
       </div>
 
@@ -47,7 +54,7 @@ function Content(props) {
         </div>
 
         <div className="vertical-line"></div>
-        
+
         <div className="score-container">
           <div className="score-value">{props.score ? props.score.max : "ไม่มีข้อมูล"}</div>
           <div className="score-label">คะแนนสูงสุด</div>
